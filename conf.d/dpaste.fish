042470998162835da660716604ebee13c1374dbf
Darren <git@darrennathanael.com>
2022-03-04 11:50:11 -0500

    fix: url update
    dpaste.de is now moved to dpaste.org.
    
    Signed-off-by: Darren <git@darrennathanael.com>

--- conf.d/dpaste.fish
+++ conf.d/dpaste.fish
@@ -1,12 +1,12 @@
 set -g __dpaste_expires_choises '(onetime|1|twotimes|2|hour|week|month|never)'
-set -g __dpaste_url_dpaste_de 'https://dpaste.de/api/'
+set -g __dpaste_url_dpaste_de 'https://dpaste.org/api/'
 set -g __dpaste_keyword_dpaste_de 'content'
 set -g __dpaste_url_sprunge_us 'http://sprunge.us/'
 set -g __dpaste_keyword_sprunge_us 'sprunge'
 set -g __dpaste_url_gist_github_com 'https://api.github.com/gists'
 set -g __dpaste_keyword_gist_github_com 'github'
 
-set -q dpaste_site; or set -g dpaste_site 'dpaste.de'
+set -q dpaste_site; or set -g dpaste_site 'dpaste.org'
 set suffix (echo $dpaste_site | sed "s/\./_/g")
 
 set -g __dpaste_url_data 'format=url'
